8b31c251a0dcffa8293e9371c1c660697307f7eb
Joe Bigio <jvbigio@gmail.com>
2020-08-14 10:20:18 -0400

    Added live clock functionality

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const timeDisplay = document.querySelector('.time-display')
-let markup = ''
 
-const today = new Date()
-// const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
-// const time = `${today.getHours()} : ${today.getMinutes()} : ${today.getSeconds()}`
-const hour = today.getHours()
-const minute = today.getMinutes()
-const seconds = today.getSeconds()
-// get AM/PM
-const amOrPm = hour => (hour < 12) ? 'AM' : 'PM'
-// convert military time
-const militaryTime = _ => (hour >= 12) ? hour - 12 : hour
+function liveClock () {
+  let markup = ''
 
-// used below in markup (original working code)
-/* <div class="time-display">${time} ${amOrPm()}</div> */
+  const today = new Date()
+  // const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+  // const time = `${today.getHours()} : ${today.getMinutes()} : ${today.getSeconds()}`
+  // auto refresh page
 
-markup += `
-  <div class="time-display">${militaryTime()}:${minute}:${seconds} ${amOrPm()}</div>
-`
-timeDisplay.innerHTML = markup
-// amOrPm()
+  const hour = today.getHours()
+  const minute = today.getMinutes()
+  const seconds = today.getSeconds()
+  // get AM/PM
+  const amOrPm = hour => (hour < 12) ? 'AM' : 'PM'
+  // convert military time
+  const militaryTime = _ => (hour >= 12) ? hour - 12 : hour
+  // change single digit minutes to 00 format
+  const doubleDigits = _ => (minute < 10) ? `0${minute}` : minute
+  // used below in markup (original working code)
+  /* <div class="time-display">${time} ${amOrPm()}</div> */
+
+  markup += `
+    <div class="time-display">${militaryTime()}:${doubleDigits()}:${seconds} ${amOrPm()}</div>
+  `
+  timeDisplay.innerHTML = markup
+  setTimeout(liveClock, 1000)
+}
+
+liveClock()
