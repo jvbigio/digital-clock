5b5b3b960616aa74644c969a6626995068726528
Joe Bigio <jvbigio@gmail.com>
2020-08-20 16:06:25 -0400

    Refactored

--- app.js
+++ app.js
@@ -1,75 +1,54 @@
-window.onload = setInterval(liveClock, 1000)
-const timeDisplay = document.querySelector('.time-display')
-const dayMonth = document.querySelector('.day-month')
-let today, hour, minute, seconds, day, month
+// main.js is refactored from app.js //
+/*
+Factor out each function so that they each do just one thing
+
+everything in liveClock is clock logic
+*/
+window.onload = () => {
+  setInterval(liveClock, 100)
+  currentDate()
+}
+
+let today, hours
 let isMilitaryTime = false
 
 function liveClock () {
-  let timeMarkup = ''
-  let dateMarkup = ''
   today = new Date()
-  hour = today.getHours()
-  minute = today.getMinutes()
-  seconds = today.getSeconds()
-  day = today.getDay()
-  month = today.getMonth()
+  const timeDisplay = document.querySelector('.time-display')
 
-  const getDayOfWeek = day => {
-    const dayOfWeek = {
-      0: 'Sunday',
-      1: 'Monday',
-      2: 'Tuesday',
-      3: 'Wednesday',
-      4: 'Thursday',
-      5: 'Friday',
-      6: 'Saturday'
-    }
-    return dayOfWeek[day]
-  }
+  hours = today.getHours()
+  let minutes = today.getMinutes()
+  let seconds = today.getSeconds()
 
-  const getMonth = month => {
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-    return months[month]
-  }
-  // get AM/PM
-  const amOrPm = _ => (hour >= 12) ? 'PM' : 'AM'
-
-  // convert military time
-  const standardTime = _ => (hour > 12) ? hour - 12 : hour
-
-  // change single digit minutes to 00 format
-  const doubleDigits = _ => (minute < 10) ? `0${minute}` : minute
-  // change single digit seconds to 00 format
-  const doubleDigitSeconds = _ => (seconds < 10) ? `0${seconds}` : seconds
+  const standardTime = hours > 12 ? hours - 12 : hours
+  minutes = minutes < 10 ? `0${minutes}` : minutes
+  seconds = seconds < 10 ? `0${seconds}` : seconds
+  const amOrPm = hours >= 12 ? 'PM' : 'AM'
 
   if (isMilitaryTime) {
-    timeMarkup += `
-    <div class="time-display">${hour}:${doubleDigits()}:${doubleDigitSeconds()} ${amOrPm()}</div>
-  `
+    timeDisplay.innerHTML = `${hours}:${minutes}:${seconds}`
     toggleButton.innerText = 'Standard Time'
   } else {
-    timeMarkup += `
-    <div class="time-display">${standardTime()}:${doubleDigits()}:${doubleDigitSeconds()} ${amOrPm()}</div>
-    `
+    timeDisplay.innerHTML = `${standardTime}:${minutes}:${seconds} ${amOrPm}`
     toggleButton.innerText = 'Military Time'
   }
+}
 
-  dateMarkup += `
-    <div class="day-month">${getDayOfWeek(day)} - ${getMonth(month)} ${today.getDate()}</div>
-  `
+function currentDate () {
+  today = new Date()
+  const dayMonth = document.querySelector('.day-month')
+  const month = today.toLocaleString('default', {
+    month: 'long'
+  })
+  const day = today.toLocaleString('default', {
+    weekday: 'long'
+  })
+  const date = today.getDate()
 
-  timeDisplay.innerHTML = timeMarkup
-  dayMonth.innerHTML = dateMarkup
+  dayMonth.innerHTML = `${day} - ${month} ${date}`
 }
 
 const toggleButton = document.getElementById('toggle-btn')
 toggleButton.addEventListener('click', function (e) {
-  if (isMilitaryTime === false) {
-    isMilitaryTime = true
-  } else {
-    isMilitaryTime = false
-  }
+  isMilitaryTime = !isMilitaryTime
 })
-
-liveClock()
